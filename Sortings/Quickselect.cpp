747bad0644569aa9ebbcbe0d9526432aa048fd75
afentev <kirill.afentyev@yandex.ru>
2021-10-30 23:36:03 +0300

    clarified quickselect

--- Sortings/Quickselect.cpp
+++ Sortings/Quickselect.cpp
@@ -18,14 +18,15 @@ int medianOf3(std::vector<int>& v, int x1, int x2, int x3) {
   return x3;
 }
 
-int orderStatic(std::vector<int> v, int k) {
+int quickSelect(std::vector<int> v, int k) {
   int l = 0;
   int r = v.size() - 1;
 
   while (true) {
     std::uniform_int_distribution<int> distr(l, r);
-    int pivotInd = medianOf3(v, distr(eng), distr(eng), distr(eng));
+    int pivotInd = medianOf3(v, distr(eng), distr(eng), distr(eng));  // median of 3 random
     int pivot = v[pivotInd];
+    // partition:
     std::swap(v[pivotInd], v[r]);
     int i = l - 1;
     for (int j = l; j <= r; ++j) {
@@ -51,7 +52,7 @@ int main() {
   std::vector<int> a {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
   std::shuffle(a.begin(), a.end(), g);
   for (int i = 0; i < a.size(); ++i) {
-    std::cout << orderStatic(a, i) << std::endl;
+    std::cout << quickSelect(a, i) << std::endl;
   }
   return 0;
 }
