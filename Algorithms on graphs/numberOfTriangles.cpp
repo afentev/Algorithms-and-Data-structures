fb06dfe2edfe3e928633352138fe411c9319324a
afentev <kirill.afentyev@yandex.ru>
2022-01-28 14:52:23 +0300

    triangles fix

--- Algorithms on graphs/numberOfTriangles.cpp
+++ Algorithms on graphs/numberOfTriangles.cpp
@@ -9,7 +9,9 @@ struct Edge {
 
 
 std::vector<std::unordered_set<int>> g;
+std::vector<std::unordered_set<int>> hv;
 std::vector<Edge> edges;
+std::vector<Edge> directedEdges;
 int trinagles = 0;
 
 // complexity is O(E√E)
@@ -23,8 +25,13 @@ void countTriangles() {
       src = e.dst;
       dst = e.src;
     }
-    for (int u: g[src]) {  // O(√E) vertexes here because src has <= neighbors than dst
-      if (g[u].contains(dst)) {
+    directedEdges.push_back({src, dst});
+    hv[src].insert(dst);
+  }
+
+  for (const Edge& e: directedEdges) {
+    for (int u: hv[e.src]) {  // Θ(√E) vertexes here because src has <= neighbors than dst
+      if (g[u].contains(e.dst)) {
         ++trinagles;
       }
     }
@@ -35,6 +42,7 @@ int main() {
   int n, m;
   std::cin >> n >> m;
   g.resize(n, std::unordered_set<int>());
+  hv.resize(n, std::unordered_set<int>());
   edges.reserve(2 * m);
 
   for (int i = 0; i < m; ++i) {  // unoriented graph
