3edc1604082c21f8d3386a2e13d56b2459e28dfa
afentev <kirill.afentyev@yandex.ru>
2022-02-10 19:33:07 +0300

    number of triangles fix

--- Algorithms on graphs/numberOfTriangles.cpp
+++ Algorithms on graphs/numberOfTriangles.cpp
@@ -1,63 +1,59 @@
 #include <iostream>
 #include <vector>
-#include <unordered_set>
-
-
-struct Edge {
-  int src, dst;
-};
 
 
 int n, m;
-std::vector<std::unordered_set<int>> g;
-std::vector<std::unordered_set<int>> hv;
-std::vector<Edge> edges;
-std::vector<Edge> directedEdges;
-int trinagles = 0;
+std::vector<std::vector<int>> g;
+std::vector<std::vector<int>> hg;
+int triangles = 0;
 
 // complexity is O(E√E)
 void countTriangles() {
-  for (const Edge& e: edges) {
-    int src, dst;
-    if (g[e.src].size() < g[e.dst].size()) {
-      src = e.src;
-      dst = e.dst;
-    } else {
-      src = e.dst;
-      dst = e.src;
-    }
-    hv[src].insert(dst);
+  for (int v = 0; v < n; ++v) {
+    std::copy_if(
+        std::begin(g[v]),
+        std::end(g[v]),
+        std::back_inserter(hg[v]),
+        [v](int a) { return !(g[v].size() < g[a].size() || (g[v].size() == g[a].size() && v < a)); }
+    );
   }
 
+  std::vector<int> cnt(n, 0);
   for (int u = 0; u < n; ++u) {
-    int cnt = 0;
-    for (int v: hv[u]) {
-      for (int w: hv[u]) {
-        if (g[v].contains(w)) {
-          trinagles++;
-        }
+    for (int w: hg[u]) {
+      cnt[w] = 0;
+    }
+    for (int v: hg[u]) {
+      for (int w: hg[v]) {
+        ++cnt[w];
       }
+    }
+    for (int w: hg[u]) {
+      triangles += cnt[w];
     }
   }
 }
 
+// solution for https://codeforces.com/group/NOwIbqv33y/contest/307527/problem/A
 int main() {
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+
   std::cin >> n >> m;
-  g.resize(n, std::unordered_set<int>());
-  hv.resize(n, std::unordered_set<int>());
-  edges.reserve(m);
+  g.resize(n, std::vector<int>());
+  hg.resize(n, std::vector<int>());
 
   for (int i = 0; i < m; ++i) {  // unoriented graph
     int u, v;
     std::cin >> u >> v;
     u--, v--;
-    edges.push_back({u, v});
-    g[u].insert(v);
-    g[v].insert(u);
+
+    g[u].push_back(v);
+    g[v].push_back(u);
   }
 
   countTriangles();
-  trinagles /= 2;
 
-  std::cout << trinagles << std::endl;
+  std::cout << triangles << std::endl;
 }
