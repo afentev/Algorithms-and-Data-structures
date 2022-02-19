8a9c2742c0bddcf192d46c060e30d9c9b434fb5a
afentev <kirill.afentyev@yandex.ru>
2022-02-19 16:39:24 +0300

    x-Fast Trie fixed

--- Trees/xFastTrie.cpp
+++ Trees/xFastTrie.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
-#include <unordered_set>
 #include <vector>
 #include <chrono>
-#include <map>
-#include <unordered_map>
 #include <set>
-#include <random>
+#include <list>
+#include <unordered_map>
 
 #include <ext/pb_ds/assoc_container.hpp>
 
 template <typename Val>
 struct Node {
   Node() = default;
-  Node(const Val& v): max(v), min(v) {}
+  Node(const typename std::list<Val>::iterator& v): max(v), min(v) {}
 
-  Val min;
-  Val max;
+  typename std::list<Val>::iterator min;
+  typename std::list<Val>::iterator max;
 };
 
 template <size_t w, typename Val>
 class xFastTrie {
   using Container = __gnu_pbds::gp_hash_table<Val, Node<Val>>;
 //  using Container = std::unordered_map<Val, Node<Val>>;  // also possible but slower
+  using listIter = typename std::list<Val>::iterator;
 
  private:
-  size_t getNodeLevel(const Val& v, int lhs = -1, int rhs = w) {
+  int getFastPrefix(const Val& v) {
+    int lhs = -1;
+    int rhs = w;
     while (rhs - lhs > 1) {
       int mid = (rhs + lhs) / 2;
       if (mapper[mid].find(v >> (w - mid - 1)) != mapper[mid].end()) {
@@ -37,79 +38,113 @@ class xFastTrie {
   }
 
  public:
-  xFastTrie() {
-    mapper.resize(w, Container());
+  xFastTrie(const Val& negInf, const Val& posInf) {
+    for (int i = 0; i < w; ++i) {
+      mapper.emplace_back();
+    }
+    list.push_back(negInf);
+    list.push_back(posInf);
   }
 
   void insert(const Val& v) {
-    Val cur = v;
-    for (int i = w - 1; i >= 0; --i) {
-      auto it = mapper[i].find(cur);
-      if (it == mapper[i].end()) {
-        mapper[i][cur] = Node(v);
-      } else {
-        Val& maxima = it->second.max;
-        Val& minima = it->second.min;
-        if (v < minima) {
-          minima = v;
-        } else if (v > maxima) {
-          maxima = v;
+    listIter it;
+    for (int level = 0; level < w; ++level) {
+      Val cur = v >> (w - level - 1);
+      if (mapper[level].find(cur) == mapper[level].end()) {
+        Val prev = cur >> 1;
+        if (level == 0) {
+          if (cur & 1) {
+            it = std::prev(list.end());
+          } else {
+            it = std::next(list.begin());
+          }
+        } else if (cur & 1) {
+          it = std::next(mapper[level - 1][prev].max);
+        } else {
+          it = mapper[level - 1][prev].min;
         }
+        it = list.insert(it, v);
+
+        for (int levelDown = level; levelDown < w; ++levelDown) {
+          mapper[levelDown][v >> (w - levelDown - 1)] = Node<Val>(it);
+        }
+
+        // step up and update min and max
+        for (int levelUp = level; levelUp >= 0; --levelUp) {
+          listIter& min = mapper[levelUp][cur].min;
+          listIter& max = mapper[levelUp][cur].max;
+          if (v < *min) {
+            min = it;
+          } else if (v > *max) {
+            max = it;
+          }
+          cur >>= 1;
+        }
+        break;
       }
-      cur >>= 1;
     }
   }
 
   void erase(const Val& v) {
-    Val cur = v;
-    for (int i = w - 1; i >= 0; --i) {
-      auto it = mapper[i].find(cur);
-      if (it->second.min == it->second.max) {
-        mapper[i].erase(cur);
-      } else if (v == it->second.min) {
-        it->second.min = predecessor(v - 1);
-      } else if (v == it->second.max) {
-        it->second.max = successor(v + 1);
+    listIter it = mapper[w - 1][v].min;  // node in list
+    listIter suc = std::next(it);
+    listIter pred = std::prev(it);
+
+    for (int level = w - 1; level >= 0; --level) {
+      Val cur = v >> (w - level - 1);
+      Node<Val>& node = mapper[level][cur];
+      if (node.min == node.max) {
+        mapper[level].erase(cur);
+      } else if (node.min == it) {
+        node.min = suc;
+      } else if (node.max == it) {
+        node.max = pred;
       }
-      it->second;
-      cur >>= 1;
+    }
+    list.erase(it);
+  }
+
+  Val successor(const Val& v) {
+    int level = getFastPrefix(v);
+
+    if (level == w - 1) {
+      return v;
+    }
+    Val prefix = v >> (w - level - 2);
+    if (prefix & 1) {
+      return *std::next(mapper[level][v >> (w - level - 1)].max);
+    } else {
+      return *mapper[level][v >> (w - level - 1)].min;
+    }
+  }
+
+  Val predecessor(const Val& v) {
+    int level = getFastPrefix(v);
+
+    if (level == w - 1) {
+      return v;
+    }
+    Val prefix = v >> (w - level - 2);
+    if (prefix & 1) {
+      return *mapper[level][v >> (w - level - 1)].max;
+    } else {
+      return *std::prev(mapper[level][v >> (w - level - 1)].min);
     }
   }
 
   bool contains(const Val& v) {
-    return getNodeLevel(v) == w - 1;
-  }
-
-  Val successor(const Val& v) {
-    size_t lhs = getNodeLevel(v);
-    Val minima = mapper[lhs][v >> (w - lhs - 1)].min;
-    if (minima >= v) {
-      return minima;
-    }
-
-    Val maxima = mapper[lhs][v >> (w - lhs - 1)].max;
-    return maxima;
-  }
-
-  Val predecessor(const Val& v) {
-    size_t lhs = getNodeLevel(v);
-    Val maxima = mapper[lhs][v >> (w - lhs - 1)].max;
-    if (maxima <= v) {
-      return maxima;
-    }
-
-    Val minima = mapper[lhs][v >> (w - lhs - 1)].min;
-    return minima;
+    return mapper[w - 1].find(v) != mapper[w - 1].end();
   }
 
   std::vector<Container> mapper;
+  std::list<Val> list;
 };
 
 void runTests() {
-  xFastTrie<32, uint32_t> t;
+  xFastTrie<32, uint32_t> t(0, UINT32_MAX);
   std::set<int> m;
 
-  int N = 1e6;
+  int N = 1e5;
   std::vector<int> d, r;
   d.reserve(N);
 
@@ -133,6 +168,26 @@ void runTests() {
   for (auto x: r) {
     assert(m.contains(x) == t.contains(x));
   }
+  for (auto x: r) {
+    auto it = m.lower_bound(x);
+    uint32_t pred, suc;
+    if (it == m.begin()) {
+      pred = 0;
+      suc = *it;
+    } else if (it == m.end()) {
+      suc = UINT32_MAX;
+      pred = *std::prev(it);
+    } else {
+      pred = *std::prev(it);
+      suc = *it;
+    }
+    if (x == *it) {
+      pred = x;
+      suc = x;
+    }
+    assert(suc == t.successor(x));
+    assert(pred == t.predecessor(x));
+  }
 
   std::cout << "All tests passed!\n" << std::endl;
 }
@@ -140,7 +195,7 @@ void runTests() {
 
 void runPerfTests() {
   const int N = 1e6;
-  xFastTrie<32, uint32_t> testXFastTrie;
+  xFastTrie<32, uint32_t> testXFastTrie(0, UINT32_MAX);
   std::set<int> testM;
 
   std::vector<int> keys, values;
