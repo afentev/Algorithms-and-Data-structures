89800c3656718921ad8ec8d9408a42513789b03d
afentev <kirill.afentyev@yandex.ru>
2022-04-20 14:30:16 +0300

    Dijkstra optimize

--- Algorithms on graphs/Shortest paths/Dijkstra.cpp
+++ Algorithms on graphs/Shortest paths/Dijkstra.cpp
@@ -12,26 +12,20 @@ void dijkstra(int v) {
   dists.resize(n, INT32_MAX);
   dists[v] = 0;
 
-  std::set<std::pair<int, int>> unprocessed;
-  for (int i = 0; i < n; ++i) {
-    if (i != v) {
-      unprocessed.insert({INT32_MAX, i});
-    } else {
-      unprocessed.insert({0, i});
-    }
-  }
-  while (!unprocessed.empty()) {
-    auto pair = *unprocessed.begin();
+  std::set<std::pair<int, int>> q;
+  q.insert({0, v});
+  while (!q.empty()) {
+    auto pair = *q.begin();
+    q.erase(q.begin());
     int dist = pair.first;
     int vertex = pair.second;
     for (const std::pair<int, int>& lookup: g[vertex]) {
-      if (dist != INT32_MAX && lookup.second + dist < dists[lookup.first]) {
-        unprocessed.erase({dists[lookup.first], lookup.first});
+      if (lookup.second + dist < dists[lookup.first]) {
+        q.erase({dists[lookup.first], lookup.first});
         dists[lookup.first] = lookup.second + dist;
-        unprocessed.insert({dists[lookup.first], lookup.first});
+        q.insert({dists[lookup.first], lookup.first});
       }
     }
-    unprocessed.erase(pair);
   }
 }
 
@@ -43,29 +37,29 @@ void dijkstraFibHeap(int v) {
 
   // we use pairing heap instead of fibonacci heap for performance reasons
   // we also could have used thin_heap_tag for thin heap
-  __gnu_pbds::priority_queue<std::pair<int, int>, std::greater<>, __gnu_pbds::pairing_heap_tag> unprocessed;
+  __gnu_pbds::priority_queue<std::pair<int, int>, std::greater<>, __gnu_pbds::pairing_heap_tag> q;
   using iterator = __gnu_pbds::priority_queue<std::pair<int, int>, std::greater<>, __gnu_pbds::pairing_heap_tag>::point_iterator;
   std::vector<iterator> table;
   table.reserve(n);
 
   for (int i = 0; i < n; ++i) {
     if (i != v) {
-      table.push_back(unprocessed.push({INT32_MAX, i}));
+      table.push_back(q.push({INT32_MAX, i}));
     } else {
-      table.push_back(unprocessed.push({0, i}));
+      table.push_back(q.push({0, i}));
     }
   }
-  while (!unprocessed.empty()) {
-    auto pair = unprocessed.top();
+  while (!q.empty()) {
+    auto pair = q.top();
+    q.erase(table[pair.second]);
     int dist = pair.first;
     int vertex = pair.second;
     for (const auto& lookup: g[vertex]) {
-      if (dist != INT32_MAX && lookup.second + dist < dists[lookup.first]) {
+      if (lookup.second + dist < dists[lookup.first]) {
         dists[lookup.first] = lookup.second + dist;
-        unprocessed.modify(table[lookup.first], std::make_pair(dists[lookup.first], lookup.first));
+        q.modify(table[lookup.first], {dists[lookup.first], lookup.first});
       }
     }
-    unprocessed.erase(table[pair.second]);
   }
 }
 
